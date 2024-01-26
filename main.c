d9534b63ecae893e0baf9a2183bac5a789085c50
Ahad <119402526+AhadHasanli@users.noreply.github.com>
2024-01-26 22:17:34 +0400

    Hello School

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++) // instead of 10
   {
-    printf("School\n");
+    printf("Hello School\n"); // instead School
   }
   return 0;
 }
